6c5a29b128164005d8376bbd0fff86ac9201f844
Ednilson Cardoso dos Santos <ednilson.csantos@mercadolivre.com>
2023-08-27 16:55:42 -0300

    chore: refactor input component export

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import * as FormGroup from './components/form';
 import Command from './components/command';
 import Dialog from './components/dialog';
 import Popover from './components/popover';
+import Input from './components/input';
 
-export {Input} from './components/input/input';
-export {InputPinCode} from './components/inputPinCode/inputPinCode';
 export {Label} from './components/label/label';
 export {Checkbox} from './components/checkbox/checkbox';
 export {Separator} from './components/separator/separator';
 export {Button} from './components/button/button';
 
-export {FormGroup, Command, Dialog, Popover};
+export {FormGroup, Command, Dialog, Popover, Input};
